ee963ead0abd7519072e1377580491be6cf4b788
Eithandarphyo51 <eithandarprogram@gmail.com>
2018-06-10 22:14:00 -0700

    =add ex18

--- ex18.py
+++ ex18.py
@@ -5,6 +5,9 @@ def print_two(*args):
     def print_two_again(arg1, arg2):
         print(f"arg1: {arg1}, arg2: {arg2}")
 
+        def print_one(arg1):
+            print(f"arg1: {arg1}")
+
         def print_none():
             print("I got nothin'.")
 
