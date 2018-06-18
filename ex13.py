a875ca5aed7cb331a62c67098d101879923daf03
Eithandarphyo51 <eithandarprogram@gmail.com>
2018-06-17 21:38:16 -0700

    added ex13

--- ex13.py
+++ ex13.py
@@ -1,6 +1,9 @@
 from sys import argv
 # read the WYSS section for to run this
-script, first, second, third = argv
+script = argv
+first = argv
+second = argv
+third = argv
 
 print("The script is called:", script)
 print("Your first variable is:", first)
