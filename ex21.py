fe810a2a56c7fb1513378bc858ad5e65cb3ef4f9
Eithandarphyo51 <eithandarprogram@gmail.com>
2018-06-17 23:57:44 -0700

    added ex21

--- ex21.py
+++ ex21.py
@@ -6,13 +6,15 @@ def subtract(a, b):
     print(f"SUBTRACTING {a} - {b}")
     return a - b
 
-def multiply(a, b)
-print(f"MULTIPLYING {a} * {b}")
-return a * b
+def multiply(a, b):
+    print(f"MULTIPLYING {a} * {b}")
+    return a * b
 
 def divide(a, b):
     print(f"DIVIDING {a} / {b}")
     return a / b
+
+
 
 print("Let's do some math with just functions!")
 
