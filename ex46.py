20083fd3403422fc1ed9db37cce914a7b5ca696b
Eithandarphyo51 <eithandarprogram@gmail.com>
2018-06-19 01:56:51 -0700

    added ex46

--- ex46.py
+++ ex46.py
@@ -2,29 +2,29 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+    
+config = {
+    'description': 'My Project',
+    'author': 'My Name',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'My email.',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'projectname'
+}
 
-    config = {
-            'description': 'My Project',
-            'author': 'My Name',
-            'url': 'URL to get it at.',
-            'download_url': 'Where to download it.',
-            'author_email': 'My email.',
-            'version': '0.1',
-            'install_requires': ['nose'],
-            'packages': ['NAME'],
-            'scripts': [],
-            'name': 'projectname'
-            }
+setup(**config)
+from nose.tools import *
+import NAME
 
-    setup(**config)
-    from nose.tools import *
-    import NAME
+def setup():
+    print("SETUP!")
 
-    def setup():
-        print("SETUP!")
+def teardown():
+    print("TEAR DOWN!")
 
-        def teardown():
-            print("TEAR DOWN!")
-
-            def text_basic():
-                print("I RAN!", end='')
+def text_basic():
+    print("I RAN!", end='')
