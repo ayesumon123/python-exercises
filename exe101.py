1faa667b07d8ca18f6128fac2018e9ae358223f0
Eithandarphyo51 <eithandarprogram@gmail.com>
2018-06-26 20:14:26 -0700

    added exe101

---
+++ exe101.py
@@ -0,0 +1,15 @@
+tabby_dog = "\tI'm tabbled in."
+persian_dog = "I'm split\non a line."
+blackslash_dog = "I'm \\ a \\ dog."
+
+fat_dog = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_dog)
+print(persian_dog)
+print(blackslash_dog)
+print(fat_dog)
